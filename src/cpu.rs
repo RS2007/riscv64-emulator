acc4d71169f25c96264c797bf97e7a8a46b72406
Rohith <rohithsuresh207@gmail.com>
2023-10-13 16:55:35 +0530

    adding the rest of the non-immediate alu instructions, not tested

--- src/cpu.rs
+++ src/cpu.rs
@@ -13,8 +13,26 @@ const ALU_OPCODE: u32 = 0b0110011;
 const ALUI_OPCODE: u32 = 0b0010011;
 const ADD_FUNCT7: u32 = 0b0000000;
 const SUB_FUNCT7: u32 = 0b0100000;
+const ADD_SUB_FUNCT3: u32 = 0b000;
+const SLL_FUNCT3: u32 = 0b001;
+const SLT_FUNCT3: u32 = 0b010;
+const SLTU_FUNCT3: u32 = 0b011;
+const XOR_FUNCT3: u32 = 0b100;
+const SRL_SRA_FUNCT3: u32 = 0b101;
+const SRL_FUNCT7: u32 = 0b0000000;
+const SRA_FUNCT7: u32 = 0b0100000;
+const OR_FUNCT3: u32 = 0b110;
+const AND_FUNCT3: u32 = 0b111;
 const ADDI_FUNCT3: u32 = 0b000;
+const SLTI_FUNCT3: u32 = 0b010;
+const SLTIU_FUNCT3: u32 = 0b011;
+const XORI_FUNCT3: u32 = 0b100;
 const ORI_FUNCT3: u32 = 0b110;
+const ANDI_FUNCT3: u32 = 0b111;
+const SLLI_FUNCT3: u32 = 0b001;
+const SRLI_SRAI_FUNCT3: u32 = 0b101;
+const SRLI_FUNCT7:u32 = 0b0000000;
+const SRAI_FUNCT7:u32 = 0b0100000;
 const LOAD_OPCODE: u32 = 0b0000011;
 const STORE_OPCODE: u32 = 0b0100011;
 const SB_FUNCT3: u32 = 0b000;
@@ -71,14 +89,62 @@ impl Cpu {
         let funct3 = ((instruction >> 12) & 0x7) as u32;
         let funct7 = ((instruction >> 25) & 0x7f) as u32;
         match opcode {
-            ALU_OPCODE => match funct7 {
-                ADD_FUNCT7 => {
-                    println!("Add: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
-                    self.regs[rd] = Cpu::alu_add(self.regs[rs1], self.regs[rs2]);
+            ALU_OPCODE => match funct3 {
+                ADD_SUB_FUNCT3 => match funct7 {
+                    ADD_FUNCT7 => {
+                        println!("Add: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
+                        self.regs[rd] = Cpu::alu_add(self.regs[rs1], self.regs[rs2]);
+                    }
+                    SUB_FUNCT7 => {
+                        println!("Sub: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
+                        self.regs[rd] = Cpu::alu_sub(self.regs[rs1], self.regs[rs2]);
+                    }
+                    _ => {
+                        todo!();
+                    }
+                },
+                SLL_FUNCT3 => {
+                    println!("SLL: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
+                    self.regs[rd] = self.regs[rs1] << (self.regs[rs2] & 0x1f);
                 }
-                SUB_FUNCT7 => {
-                    println!("Sub: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
-                    self.regs[rd] = Cpu::alu_sub(self.regs[rs1], self.regs[rs2]);
+                SLT_FUNCT3 => {
+                    println!("SLT: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
+                    self.regs[rd] = match (self.regs[rs1] as i32) < (self.regs[rs2] as i32) {
+                        true => 1,
+                        false => 0,
+                    };
+                }
+                SLTU_FUNCT3 => {
+                    println!("SLTU: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
+                    self.regs[rd] = match (self.regs[rs1] as u32) < (self.regs[rs2] as u32) {
+                        true => 1,
+                        false => 0,
+                    };
+                }
+                XOR_FUNCT3 => {
+                    println!("SLTU: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
+                    self.regs[rd] = self.regs[rs1] ^ self.regs[rs2];
+                }
+                SRL_SRA_FUNCT3 => match funct7 {
+                    SRL_FUNCT7 => {
+                        println!("SRL: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
+                        self.regs[rd] = (self.regs[rs1] as u32) >> ((self.regs[rs2] & 0x1f) as u32);
+                    }
+                    SRA_FUNCT7 => {
+                        println!("SRA: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
+                        self.regs[rd] = (self.regs[rs1] as i32).wrapping_shr(self.regs[rs2]) as u32;
+                    }
+                    _ => {
+                        assert!(false, "Should'nt hit this");
+                    }
+                },
+                OR_FUNCT3 => {
+                    println!("OR: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
+                    self.regs[rd] = (self.regs[rs1]) | (self.regs[rs2]);
+                }
+                AND_FUNCT3 => {
+                    println!("AND: rs1={:?} rs2={:?} rd={:?}", rs1, rs2, rd);
+                    self.regs[rd] = (self.regs[rs1]) & (self.regs[rs2]);
                 }
                 _ => {
                     todo!();
@@ -90,13 +156,57 @@ impl Cpu {
                     println!("Add: rs1={:?} imm={:?} rd={:?}", rs1, imm, rd);
                     self.regs[rd] = Cpu::alu_add(self.regs[rs1], imm);
                 }
+                SLTI_FUNCT3 => {
+                    let imm = Cpu::sign_extend((instruction & 0xfff00000) >> 20);
+                    println!("Slti: rs1={:?} imm={:?} rd={:?}", rs1, imm, rd);
+                    self.regs[rd] = match (i32::try_from(self.regs[rs1]).unwrap())
+                        < (i32::try_from(imm).unwrap())
+                    {
+                        true => 1,
+                        false => 0,
+                    };
+                }
+                SLTIU_FUNCT3 => {
+                    let imm = Cpu::sign_extend((instruction & 0xfff00000) >> 20);
+                    println!("Sltiu: rs1={:?} imm={:?} rd={:?}", rs1, imm, rd);
+                    self.regs[rd] = match self.regs[rs1] < imm {
+                        true => 1,
+                        false => 0,
+                    };
+                }
+                XORI_FUNCT3 => {
+                    let imm = Cpu::sign_extend((instruction & 0xfff00000) >> 20);
+                    println!("Xori: rs1={:?} imm={:?} rd={:?}", rs1, imm, rd);
+                    self.regs[rd] = self.regs[rs1] | imm;
+                }
                 ORI_FUNCT3 => {
                     let imm = Cpu::sign_extend((instruction & 0xfff00000) >> 20);
                     println!("Or: rs1={:?} imm={:?} rd={:?}", rs1, imm, rd);
                     self.regs[rd] = self.regs[rs1] | imm;
                 }
+                ANDI_FUNCT3 => {
+                    let imm = Cpu::sign_extend((instruction & 0xfff00000) >> 20);
+                    println!("And: rs1={:?} imm={:?} rd={:?}", rs1, imm, rd);
+                    self.regs[rd] = self.regs[rs1] & imm;
+                }
+                SLLI_FUNCT3 => {
+                    let imm = Cpu::sign_extend((instruction & 0xfff00000) >> 20);
+                    println!("Slli: rs1={:?} imm={:?} rd={:?}", rs1, imm, rd);
+                    self.regs[rd] = self.regs[rs1] << imm;
+                }
+                SRLI_SRAI_FUNCT3 => match funct7 {
+                    SRLI_FUNCT7=>{
+                        todo!();
+                    },
+                    SRAI_FUNCT7=>{
+                        todo!();
+                    }
+                    _ => {
+                        todo!();
+                    }
+                },
                 _ => {
-                    todo!();
+                    assert!(false,"Should'nt hit this");
                 }
             },
             LOAD_OPCODE => match funct3 {
